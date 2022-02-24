866d1810d7331c0af29650b95f5f089b96770786
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-02-24 22:09:15 +0100

    v1.0

---
+++ compile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##############################
+# For linux only. Usage:
+# ./compile.sh FILE.c [run]
+##############################
+
+# Check if sdl2 is installed
+if [[ $(command -v sdl2-config) == "" ]]; then
+	echo "Cant' find sdl2. Exiting..."
+	exit 1;
+fi
+
+SDL_FLAGS=$(sdl2-config --cflags --libs)
+OUTPUT_FILE="langtons-ant.o"  # Change with project name
+
+if [[ $1 == "" ]]; then
+	echo "No arguments. Please enter a file. Exiting..."
+	exit 1;
+fi
+
+# Compile the file
+gcc -o $OUTPUT_FILE $1 $SDL_FLAGS
+
+# If the second arg is run then run after compiling
+if [[ $2 == "run" ]]; then
+	echo "Compiled! Running..."
+	./$OUTPUT_FILE
+fi
