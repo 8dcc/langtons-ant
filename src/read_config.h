0cba6d7e784fd4a0a9f7da2e18e2bbfde1490c8f
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-03-07 20:42:23 +0100

    v1.4

--- src/read_config.h
+++ src/read_config.h
@@ -1,5 +1,25 @@
 #define CONFIG_BUFFER_SIZE 255
-#define DEBUG_SETTINGS 1
+#define DEBUG_SETTINGS 0
+
+int WINDOW_W = 1200, WINDOW_H = 750;
+int FPS = 60, DELAY = 50;
+int DEBUG_PRINT = 0;
+
+int parse_setting(char setting[], int value) {
+	if (compare_strings(setting, "WINDOW_W") || compare_strings(setting, "window_w")) {
+		WINDOW_W = value;
+	} else if (compare_strings(setting, "WINDOW_H") || compare_strings(setting, "window_h")) {
+		WINDOW_H = value;
+	} else if (compare_strings(setting, "FPS") || compare_strings(setting, "fps")) {
+		FPS = value;
+	} else if (compare_strings(setting, "DELAY") || compare_strings(setting, "delay")) {
+		DELAY = value;
+	} else if (compare_strings(setting, "DEBUG_PRINT") || compare_strings(setting, "debug_print")) {
+		DEBUG_PRINT = value;
+	}
+
+	return 0;
+}
 
 int read_config() {
 	char line[CONFIG_BUFFER_SIZE], setting[CONFIG_BUFFER_SIZE];
@@ -14,6 +34,7 @@ int read_config() {
 		sscanf(line, "%s = %d", setting, &value);
 		if (setting[0] != '#') {	// Ignore comments
 			if (DEBUG_SETTINGS) printf("Found setting: %10s | %d\n", setting, value);
+			parse_setting(setting, value);
 		}
 	}
 
@@ -21,15 +42,3 @@ int read_config() {
 	return 0;
 }
 
-int parse_setting(char setting, int value) {
-	// TODO
-	switch (setting) {
-		case "a":
-			a = value;
-			break;
-		default:
-			break;
-	}
-
-	return 0;
-}
