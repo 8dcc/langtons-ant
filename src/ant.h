272f0d37c97ebfb85641672bfbe7cbbae8000aa7
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-03-15 18:52:03 +0100

    v1.12

--- src/ant.h
+++ src/ant.h
@@ -1,6 +1,9 @@
 
 enum directions { UP, DOWN, LEFT, RIGHT };
 
+// x_pos, y_pos, facing_dir
+int ANT_STATE[3] = {  };
+
 // TODO: Will need to store the currents ant position in an array in main.c
 void move_forward(int facing_direction, int ant_position_array[]) {
 	switch (facing_direction) {
@@ -30,13 +33,13 @@ int rotate(int rotation, int facing_direction) {
 				break;
 			case DOWN:
 				return LEFT;
-				break
+				break;
 			case LEFT:
 				return UP;
-				break
+				break;
 			case RIGHT:
 				return DOWN;
-				break
+				break;
 			default:
 				break;
 		}
@@ -47,19 +50,29 @@ int rotate(int rotation, int facing_direction) {
 				break;
 			case DOWN:
 				return RIGHT;
-				break
+				break;
 			case LEFT:
 				return DOWN;
-				break
+				break;
 			case RIGHT:
 				return UP;
-				break
+				break;
 			default:
 				break;
 		}
 	}
 
 	return 1;
+}
+
+void draw_grid(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, GRID_COLOR, GRID_COLOR, GRID_COLOR, 255);
+	for (int x_grid = CELL_SIZE; x_grid < WINDOW_W; x_grid = x_grid + CELL_SIZE) {
+		SDL_RenderDrawLine(renderer, x_grid, 0, x_grid, WINDOW_H);
+	}
+	for (int y_grid = CELL_SIZE; y_grid < WINDOW_H; y_grid = y_grid + CELL_SIZE) {
+		SDL_RenderDrawLine(renderer, 0, y_grid, WINDOW_W, y_grid);
+	}
 }
 
 // TODO: Function to return rotation based on current color? or just main.c
