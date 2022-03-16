ec8ee1353e72d5515821a3fb94bbaf14a672e293
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-03-17 00:19:19 +0100

    v1.18
    Added a ton of shit but a lot doesn't work sorry I am tired

--- src/ant.h
+++ src/ant.h
@@ -1,23 +1,23 @@
 
 enum directions { UP, DOWN, LEFT, RIGHT };
 
-// x_pos, y_pos, facing_dir
+// y_pos, x_pos, facing_dir
 int ANT_STATE[3];
 
 // TODO: Will need to store the currents ant position in an array in main.c
-void move_forward(int facing_direction, int ant_position_array[]) {
+void move_forward(int facing_direction) {
 	switch (facing_direction) {
 		case UP:
-			ANT_STATE[1]++;
+			if (ANT_STATE[1] < WINDOW_W/CELL_SIZE) ANT_STATE[1]++;
 			break;
 		case DOWN:
-			ANT_STATE[1]--;
+			if (ANT_STATE[1] > 0) ANT_STATE[1]--;
 			break;
 		case LEFT:
-			ANT_STATE[0]--;
+			if (ANT_STATE[0] > 0) ANT_STATE[0]--;
 			break;
 		case RIGHT:
-			ANT_STATE[0]++;
+			if (ANT_STATE[0] < WINDOW_H/CELL_SIZE) ANT_STATE[0]++;
 			break;
 		default:
 			break;
@@ -77,9 +77,99 @@ void draw_grid(SDL_Renderer* renderer) {
 
 void start_ant() {
 	// Start at the center of the screen, facing up
-	ANT_STATE[0] = WINDOW_W/CELL_SIZE;
-	ANT_STATE[1] = WINDOW_H/CELL_SIZE; 
+	ANT_STATE[0] = WINDOW_H/CELL_SIZE / 2;
+	ANT_STATE[1] = WINDOW_W/CELL_SIZE / 2; 
 	ANT_STATE[2] = UP;
 }
 
-// TODO: Function to return rotation based on current color? or just main.c
+int move_ant(int cell_array[]) {
+	// Based on color, 0 is ClockWise, 1 is CounterClockWise.
+	// Currently turns CW if the color number is even, CCW otherwise.
+	int current_color = cell_array[ANT_STATE[0], ANT_STATE[1]];
+
+	if (current_color % 2 == 0) {	// Number is even
+		ANT_STATE[3] = rotate(0, ANT_STATE[3]);
+	} else {
+		ANT_STATE[3] = rotate(1, ANT_STATE[3]);
+	}
+
+	// Change the color of the current cell based on the previous one
+	if (current_color == 0) {
+		cell_array[ANT_STATE[0], ANT_STATE[1]] = COLORS_ARRAY[0];
+	} else {
+		for (int n = 0; n < MAX_COLOR_NUMBER; n++) {
+			if (current_color = COLORS_ARRAY[n]) {
+				if (n+1 >= COLOR_NUMBER) {
+					cell_array[ANT_STATE[0], ANT_STATE[1]] = COLORS_ARRAY[0];
+				} else {
+					cell_array[ANT_STATE[0], ANT_STATE[1]] = COLORS_ARRAY[n+1];
+				}
+				break;
+			}
+		}
+	}
+
+
+	// Move forward once
+	move_forward(ANT_STATE[3]);
+
+	printf("Moved ant! [%d, %d]\n", ANT_STATE[0], ANT_STATE[1]);
+
+	return 0;
+}
+
+// Given a color and an array, will write the RGB values of that color to the array
+int rgb_from_color(int color, int rgb_buffer[]) {
+	switch (color) {
+		case 1:
+			rgb_buffer[0] = 0;
+			rgb_buffer[1] = 0;
+			rgb_buffer[2] = 0;
+			return 0;
+		case 2:
+			rgb_buffer[0] = 255;
+			rgb_buffer[1] = 255;
+			rgb_buffer[2] = 255;
+			return 0;
+		case 3:
+			rgb_buffer[0] = 255;
+			rgb_buffer[1] = 0;
+			rgb_buffer[2] = 0;
+			return 0;
+		case 4:
+			rgb_buffer[0] = 255;
+			rgb_buffer[1] = 165;
+			rgb_buffer[2] = 0;
+			return 0;
+		case 5:
+			rgb_buffer[0] = 255;
+			rgb_buffer[1] = 255;
+			rgb_buffer[2] = 0;
+			return 0;
+		case 6:
+			rgb_buffer[0] = 0;
+			rgb_buffer[1] = 255;
+			rgb_buffer[2] = 0;
+			return 0;
+		case 7:
+			rgb_buffer[0] = 0;
+			rgb_buffer[1] = 0;
+			rgb_buffer[2] = 255;
+			return 0;
+		case 8:
+			rgb_buffer[0] = 160;
+			rgb_buffer[1] = 32;
+			rgb_buffer[2] = 240;
+			return 0;
+		case 9:
+			rgb_buffer[0] = 255;
+			rgb_buffer[1] = 68;
+			rgb_buffer[2] = 204;
+			return 0;
+		default:
+			return 1;
+			break;
+	}
+}
+
+
