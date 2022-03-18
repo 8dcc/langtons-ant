79175997e5959966480e8b77b1f162b38911f2e2
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-03-18 17:53:30 +0100

    v1.29

--- src/main.c
+++ src/main.c
@@ -68,13 +68,17 @@ int main(int argc, char* argv[]) {
 	/* ------------------------------------------------------------------------------- */
 
 	// Main loop
-	int running = 1, space_pressed = 0, grid_active = 0;
+	int running = 1, grid_active = 0;
+	int space_pressed = 0, r_arrow_pressed = 0;
+	int number_of_steps = 0;
 	int rgb_buffer[3] = {0,0,0};
 
 	SDL_Rect current_cell;
 
 	SDL_Event sdl_event;	// Create an event for the keys and shit
 	while (running == 1) {
+		if (r_arrow_pressed) r_arrow_pressed = 0;		// Restore arrow state so it executes once only
+		
 		// Events
 		while (SDL_PollEvent(&sdl_event)) {
 			switch (sdl_event.type) {
@@ -96,6 +100,8 @@ int main(int argc, char* argv[]) {
 						case SDL_SCANCODE_SPACE:
 							space_pressed = !space_pressed;
 							break;
+						case SDL_SCANCODE_RIGHT:
+							r_arrow_pressed = 1;
 						default:
 							break;
 					}
@@ -127,16 +133,23 @@ int main(int argc, char* argv[]) {
 		SDL_RenderClear(sdl_renderer);
 
 		// Do something every frame the space is pressed
-		if (space_pressed) {
-			move_ant(&cell_grid[0][0], WINDOW_H/CELL_SIZE, WINDOW_W/CELL_SIZE);	// See comment before this funtion in ant.h
+		if (space_pressed || r_arrow_pressed) {
+			move_ant(&cell_grid[0][0], WINDOW_W/CELL_SIZE);	// See comment before this funtion in ant.h
+
+			// Show the number of steps in the terminal
+			if (DEBUG_PRINT != 2) {
+				number_of_steps++;
+				printf("\rSteps: %5d", number_of_steps);
+				fflush(stdout);
+			}
 		}
 	
 		// Draw the cells
 		for (int y = 0; y < WINDOW_H/CELL_SIZE; y++) {
 			for (int x = 0; x < WINDOW_W/CELL_SIZE; x++) {
 				// Only draw colors if the cell is not empty (background)
-				if (cell_grid[y][x] > 0) {
-					if (rgb_from_color(cell_grid[y][x], rgb_buffer) && DEBUG_PRINT) printf("There was an error while parsing RGB values...\n");
+				if (cell_grid[y][x] > BACKGROUND) {
+					if (rgb_from_color(cell_grid[y][x], rgb_buffer) && DEBUG_PRINT) printf("\nThere was an error while parsing RGB values...\n");
 					SDL_SetRenderDrawColor(sdl_renderer, rgb_buffer[0], rgb_buffer[1], rgb_buffer[2], 255);
 					current_cell.x = x * CELL_SIZE;
 					current_cell.y = y * CELL_SIZE;
@@ -151,12 +164,11 @@ int main(int argc, char* argv[]) {
 		if (DEBUG_PRINT == 2) printf("------------------------------------------------------------------\n");
 
 		// Draw the ant
-		// TODO
 		SDL_SetRenderDrawColor(sdl_renderer, 150, 0, 0, 255);
 		current_cell.x = ANT_STATE[1] * CELL_SIZE + 1;
 		current_cell.y = ANT_STATE[0] * CELL_SIZE + 1;
-		current_cell.w = CELL_SIZE - 1;
-		current_cell.h = CELL_SIZE - 1;
+		current_cell.w = CELL_SIZE - 2;
+		current_cell.h = CELL_SIZE - 2;
 		SDL_RenderFillRect(sdl_renderer, &current_cell);
 
 		// Draw grid if active
@@ -172,6 +184,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	// End of the program
+	if (DEBUG_PRINT != 2) printf("\n");
 	if (DEBUG_PRINT) printf("Reached end of the program!\n");
 	SDL_DestroyRenderer(sdl_renderer);
 	SDL_DestroyWindow(sdl_window);
