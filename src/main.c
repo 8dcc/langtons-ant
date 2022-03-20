0841704f09b58e68604274bb134101045743d562
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-03-20 21:01:33 +0100

    v1.32 Changed the way DEBUG_PRINT (vervose level) works. Check readme.md

--- src/main.c
+++ src/main.c
@@ -19,14 +19,14 @@ int main(int argc, char* argv[]) {
 		printf("Error opening config.cfg\nExiting...\n");
 		return 1;
 	}
-	if (DEBUG_PRINT) printf("Configuration loaded!\n");
+	if (DEBUG_PRINT >= 2) printf("Configuration loaded!\n");
 
 	// Start SDL
 	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
 		SDL_Log("Unable to start: %s\n", SDL_GetError());
 		return 1;
 	}
-	if (DEBUG_PRINT) printf("SLD started!\n");
+	if (DEBUG_PRINT >= 2) printf("SLD started!\n");
 	
 	// Create window
 	SDL_Window* sdl_window = SDL_CreateWindow("Langton's ant",
@@ -36,7 +36,7 @@ int main(int argc, char* argv[]) {
 		SDL_Quit();
 		return 1;
 	}
-	if (DEBUG_PRINT) printf("Window created!\n");
+	if (DEBUG_PRINT >= 2) printf("Window created!\n");
 
 	// Create renderer
 	Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
@@ -47,12 +47,12 @@ int main(int argc, char* argv[]) {
 		SDL_Quit();
 		return 1;
 	}
-	if (DEBUG_PRINT) printf("Renderer created!\n\n");
+	if (DEBUG_PRINT >= 2) printf("Renderer created!\n");
 	
 	start_ant();
-	if (DEBUG_PRINT) printf("Ant created!\n\n");
+	if (DEBUG_PRINT >= 2) printf("Ant created!\n");
 
-	printf("Press space to toggle the simulation.\n");
+	if (DEBUG_PRINT >= 1) printf("Press space to toggle the simulation.\n\n");
 
 	/* ------------------------------------------------------------------------------- */
 
@@ -90,9 +90,6 @@ int main(int argc, char* argv[]) {
 					switch (sdl_event.key.keysym.scancode) {
 						case SDL_SCANCODE_ESCAPE:
 							running = 0;
-							if (DEBUG_PRINT == 1) {
-								printf("Esc key pressed!\n");
-							}
 							break;
 						case SDL_SCANCODE_G:
 							grid_active = !grid_active;
@@ -137,9 +134,12 @@ int main(int argc, char* argv[]) {
 			move_ant(&cell_grid[0][0], WINDOW_W/CELL_SIZE);	// See comment before this funtion in ant.h
 
 			// Show the number of steps in the terminal
-			if (DEBUG_PRINT != 2) {
+			if (DEBUG_PRINT == 1 || DEBUG_PRINT == 2) {
 				number_of_steps++;
-				printf("\rSteps: %5d", number_of_steps);
+				printf("\rSteps: %d", number_of_steps);
+				if (DEBUG_PRINT == 2) {
+					printf(" | Ant1: %d-%d ", ANT_STATE[0], ANT_STATE[1]);
+				}
 				fflush(stdout);
 			}
 		}
@@ -149,7 +149,7 @@ int main(int argc, char* argv[]) {
 			for (int x = 0; x < WINDOW_W/CELL_SIZE; x++) {
 				// Only draw colors if the cell is not empty (background)
 				if (cell_grid[y][x] > BACKGROUND) {
-					if (rgb_from_color(cell_grid[y][x], rgb_buffer) && DEBUG_PRINT) printf("\nThere was an error while parsing RGB values...\n");
+					if (rgb_from_color(cell_grid[y][x], rgb_buffer) && DEBUG_PRINT >= 2) printf("\nThere was an error while parsing RGB values...\n");
 					SDL_SetRenderDrawColor(sdl_renderer, rgb_buffer[0], rgb_buffer[1], rgb_buffer[2], 255);
 					current_cell.x = x * CELL_SIZE;
 					current_cell.y = y * CELL_SIZE;
@@ -157,11 +157,14 @@ int main(int argc, char* argv[]) {
 					current_cell.h = CELL_SIZE;
 					SDL_RenderFillRect(sdl_renderer, &current_cell);
 				}
-				if ( DEBUG_PRINT == 2 && y > WINDOW_H/CELL_SIZE/2 - 5 && y < WINDOW_H/CELL_SIZE/2 + 5 ) printf("%d", cell_grid[y][x]);
+				if ( DEBUG_PRINT >= 3 && y > WINDOW_H/CELL_SIZE/2 - 5 && y < WINDOW_H/CELL_SIZE/2 + 5 &&
+						x > WINDOW_W/CELL_SIZE/2 - 5 && x < WINDOW_W/CELL_SIZE/2 + 10) {
+					printf("%d", cell_grid[y][x]);
+				}
 			}
-			if ( DEBUG_PRINT == 2 && y > WINDOW_H/CELL_SIZE/2 - 5 && y < WINDOW_H/CELL_SIZE/2 + 5 ) printf("\n");
+			if ( DEBUG_PRINT >= 3 && y > WINDOW_H/CELL_SIZE/2 - 5 && y < WINDOW_H/CELL_SIZE/2 + 5 ) printf("\n");
 		}
-		if (DEBUG_PRINT == 2) printf("------------------------------------------------------------------\n");
+		if (DEBUG_PRINT >= 3) printf("------------------------------------------------------------------\n");
 
 		// Draw the ant
 		SDL_SetRenderDrawColor(sdl_renderer, 150, 0, 0, 255);
@@ -184,8 +187,8 @@ int main(int argc, char* argv[]) {
 	}
 
 	// End of the program
-	if (DEBUG_PRINT != 2) printf("\n");
-	if (DEBUG_PRINT) printf("Reached end of the program!\n");
+	if (DEBUG_PRINT) printf("\n");
+	if (DEBUG_PRINT >= 2) printf("Reached end of the program!\n");
 	SDL_DestroyRenderer(sdl_renderer);
 	SDL_DestroyWindow(sdl_window);
 	SDL_Quit();
